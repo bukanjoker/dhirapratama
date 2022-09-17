9d9ae88fc0736387871eef5c7258c00727dd8196
bukanjoker <bukanjoker@rocketmail.com>
2022-09-18 00:51:52 +0700

    feat: test config

--- resources/views/pages/config.blade.php
+++ resources/views/pages/config.blade.php
@@ -6,11 +6,14 @@
   </head>
   <body>
     <div class="">
-      <form action="/config/{{ $config->key }}" method="post">
+      <form action="/config/{{ $config->key }}" method="post" style="padding-bottom: 24px;">
         @csrf
         <input type="text" name="value" placeholder="{{ $config->label }}" value="{{ $config->value ?? '' }}">
         <button type="submit">Submit</button>
       </form>
+      @if($config->key == 'youtube')
+        <a href="{{ $config->value }}" target="_blank">link test</a>
+      @endif
     </div>
   </body>
 </html>
